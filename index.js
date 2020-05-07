da354b2698fe3ab430b5e5f6720a24a3e9c6ac2a
Serena Tay <tays@oregonstate.edu>
2020-05-06 19:43:27 -0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + 'public')));
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
